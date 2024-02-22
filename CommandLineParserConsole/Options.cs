bf69e2383f097f0cb39d1acaee694744c483c57e
Emanuel Gonthier <egonthier@morneaushepell.com>
2024-02-22 14:46:36 -0500

    CommandLineParser

---
+++ CommandLineParserConsole/Options.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace CommandLineParserConsole;
+
+internal class Options
+{
+    [Option('v', "verbose", Required = false, HelpText = "Enable verbose output.")]
+    public bool Verbose { get; set; }
+
+    [Option('n', "name", Required = true, HelpText = "Name input.")]
+    public required string Name { get; set; }
+
+    [Option('b', "birth", Required = false, HelpText = "Birth date input.")]
+    public DateTime? BirhtDate { get; set; }
+
+    [Option('c', "color", Required = false, Default = Color.Red, HelpText = "Favorite color.")]
+    public Color FavoriteColor { get; set; }
+}
+
+internal enum Color
+{
+    Red,
+    Blue,
+    Green,
+    Yellow,
+    Purple,
+    Orange
+}
--- CommandLineParserConsole/Program.cs
+++ CommandLineParserConsole/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CommandLine;
+using CommandLineParserConsole;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(options =>
+    {
+        if (options.Verbose)
+        {
+            Console.WriteLine($"Your name is {options.Name}");
+            if (options.BirhtDate is not null) Console.WriteLine($"Your birth date is {options.BirhtDate:D}");
+            Console.WriteLine($"Your favorite color is: {options.FavoriteColor}");
+        }
+        else
+        {
+            Console.WriteLine($"Name: {options.Name}");
+            if (options.BirhtDate is not null) Console.WriteLine($"Birth date: {options.BirhtDate:d}");
+            Console.WriteLine($"Favorite color: {options.FavoriteColor}");
+        }
+    });
+;
